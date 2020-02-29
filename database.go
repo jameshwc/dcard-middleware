846e66eb1de4e62411ec79177287e5ef7f466436
jameshwc <b07902001@csie.ntu.edu.tw>
2020-02-29 18:55:30 +0800

    separate database interface logic from redis.go to database.go

---
+++ database.go
@@ -0,0 +1,14 @@
+package main
+
+type Database interface {
+	// Connect to the database server with defined maxIP and timeout
+	Init(int, int) error
+	// Check if the IP is in database and whether it's forbidden or not
+	Find(string) (bool, bool)
+	// return X-RateLimit-Remaining and X-RateLimit-Reset
+	GetKey(string) (int, string, error)
+	// If IP is not found in database, then create one
+	SetKey(string) error
+	// Increment the visit counter of the IP
+	IncrementVisitByIP(string) error
+}
--- redis.go
+++ redis.go
@@ -12,19 +12,6 @@ const dbConfigFile = "db.toml"
 
 // This project uses Redis.
 
-type Database interface {
-	// Connect to the database server with defined maxIP and timeout
-	Init(int, int) error
-	// Check if the IP is in database and whether it's forbidden or not
-	Find(string) (bool, bool)
-	// return X-RateLimit-Remaining and X-RateLimit-Reset
-	GetKey(string) (int, string, error)
-	// If IP is not found in database, then create one
-	SetKey(string) error
-	// Increment the visit counter of the IP
-	IncrementVisitByIP(string) error
-}
-
 type redisServer struct {
 	Config struct {
 		Host, Name, Password string
